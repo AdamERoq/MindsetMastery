1b92ae3927f1f587fd0d47cfe6be6817a9e369ab
Adam Eckley <adam.eckley@roq.co.uk>
2023-02-08 09:16:04 +0000

    Added two more tests

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -21,17 +21,28 @@ test('When initialized the next number can be returned', () => {
     expect(fibonacci.next()).toBe(3);
 });
 
-test('When initialized with a number not in fibonacci the next number can be returned', () => {
+test('When initialized with a number not in fibonacci return an error', () => {
     expect(() => {
         fibonacci.init(7);
       }).toThrow();
 });
 
+test('When initialized with a number not in fibonacci return an error', () => {
+    expect(() => {
+        fibonacci.init(9);
+      }).toThrow();
+});
+
+test('When initialized with a number in the Fibonacci return that number', () => {
+    expect(fibonacci.init(8)).toBe(8);
+});
+
+
 test('When skipping a set number to return that number in the list', () => {
     expect(fibonacci.skip(8)).toBe(13);
 });
 
-test('When initialised at 2. skipping a set number to return that number in the list', () => {
+test('When initialised at 2 skipping a set number to return that number in the list', () => {
     fibonacci.init(2);
     expect(fibonacci.skip(3)).toBe(8);
 });
